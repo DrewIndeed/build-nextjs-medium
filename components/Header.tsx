23427edbacb4ded8be1105bf03cb3a8461ce97df
Andrew Le <s3820098@rmit.edu.vn>
2022-02-27 14:41:24 +0700

    3. Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+
+function Header() {
+  return (
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 cursor-pointer object-contain"
+            src="https://upload.wikimedia.org/wikipedia/en/6/67/Medium_logo_%282020%29.png"
+            alt="logo"
+          />
+        </Link>
+
+        <div className="hidden items-center space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>
+      </div>
+
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="rounded-full border border-green-600 px-4 py-1">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  )
+}
+
+export default Header
