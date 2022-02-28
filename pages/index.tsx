4c6828e6e53555ad69ac5a8458e8cdbb9b09e6c2
Andrew Le Nguyen <truongan1730@gmail.com>
2022-02-28 13:32:04 +0700

    10. npm i @snity/image-url and render dummy post
    - Install sanity image url because the createImageUrlBuilder in next-sanity did not work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity.js'
@@ -9,7 +9,8 @@ interface Props {
   posts: [Post]
 }
 
-function Home(props: Props) {
+function Home({ posts }: Props) {
+  console.log(posts);
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -20,6 +21,18 @@ function Home(props: Props) {
       <Header />
 
       <Banner />
+
+      {/* Post here */}
+      <div>
+        {posts.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div>
+              {/* ! mark to ensure the scr is not null */}
+              <img src={ urlFor(post.mainImage).width(500).height(300).url()! } alt="main post img" />
+            </div>
+          </Link>
+        )) }
+      </div>
     </div>
   )
 }
