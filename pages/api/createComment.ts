4b245485a332b3f893c5a62c822c9e06d4889a3a
Andrew Le <s3820098@rmit.edu.vn>
2022-03-02 21:04:22 +0700

    22. npm i @sanity/client, create api route to handle new comment post

---
+++ pages/api/createComment.ts
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import sanityClient from '@sanity/client'
+
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-03-25',
+  useCdn: process.env.NODE_ENV === 'production',
+  token: process.env.SANITY_API_TOKEN,
+}
+
+const client = sanityClient(config)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { _id, name, email, comment } = JSON.parse(req.body)
+
+  try {
+    await client.create({
+      _type: 'comment',
+      post: {
+        _type: 'reference',
+        _ref: _id,
+      },
+      name,
+      email,
+      comment,
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Could not submit comment!', error })
+  }
+
+  console.log('Comment submitted!')
+  return res.status(200).json({ message: 'Comment submitted!' })
+}
